2af9eb4b260a80b67d10f3d116f0b5d997065d4a
hoang_imac <hoangnh5901@outlook.com>
2020-08-21 21:16:39 +0700

    no message

--- app.js
+++ app.js
@@ -1,47 +1,39 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 require("dotenv/config");
-
 const { exec } = require("child_process");
-
 app.use(bodyParser.json());
-// connect to DB
-// mongoose.connect(
-//   process.env.DB_CONNECTION,
-//   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-//   (err) => {
-//     if (err) console.log(err);
-//     else console.log("connect to mongoDB success");
-//   }
-// );
-//routes
-const juiceRouter = require("./routes/juice");
-const userRouter = require("./routes/user");
-const noteRouter = require("./routes/note");
-// app.use("/juice", juiceRouter);
-// app.use("/", userRouter);
-// app.use("/CRUD", noteRouter);
 app.get("/", (req, res) => {
   res.json({
     msg: "success",
   });
 });
 
-app.get("/aws", (req, res) => {
-  const { Bucket, Name } = req.query;
-  const url = `https://${Bucket}.s3.amazonaws.com/${Name}`;
+app.post("/aws", async (req, res) => {
   try {
-    exec(
-      ` aws rekognition detect-custom-labels \
-    --project-version-arn "arn:aws:rekognition:us-east-1:655053471542:project/custom-lables/version/custom-lables.2020-08-20T17.01.00/1597917661204" \
-    --image '{"S3Object": {"Bucket":"${Bucket}","Name": "${Name}"}}' \
-    --region us-east-1`,
-      (err, out, outErr) => {
-        res.json({ url, ...JSON.parse(out) });
-      }
-    );
+    const Name = `sac_${new Date().getTime()}.jpg`;
+    var base64Data = req.body.payload.replace(/^data:image\/png;base64,/, "");
+    require("fs").writeFile("images/" + Name, base64Data, "base64", function (
+      err
+    ) {
+      exec(
+        "cd images && ls && aws s3 sync . s3://store-wind",
+        (err, out, outErr) => {
+          const Bucket = "store-wind";
+          const url = `https://${Bucket}.s3.amazonaws.com/${Name}`;
+          exec(
+            `aws rekognition detect-custom-labels \
+          --project-version-arn "arn:aws:rekognition:us-east-1:655053471542:project/cac_loai_sac/version/cac_loai_sac.2020-08-21T17.14.49/1598004889458" \
+          --image '{"S3Object": {"Bucket": "${Bucket}","Name": "${Name}"}}' \
+          --region us-east-1`,
+            (err, out, outErr) => {
+              res.json({ url, ...JSON.parse(out) });
+            }
+          );
+        }
+      );
+    });
   } catch (error) {
     res.send(error);
   }
