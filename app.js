4e850ca205aa8523e6982fe879ba0bb5c19fa6c7
Hoàng <hoangnh5901@outlook.com>
2020-08-27 17:31:49 +0700

    add upload by form data

--- app.js
+++ app.js
@@ -2,51 +2,48 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const AWS = require("aws-sdk");
+const fileupload = require("express-fileupload");
 require("dotenv/config");
-const { exec } = require("child_process");
 app.use(bodyParser.json());
+app.use(fileupload());
 app.get("/", (req, res) => {
   res.json({
-    msg: "success",
+    msg: "aws demo",
   });
 });
 
 app.post("/aws", async (req, res) => {
   var TimeStart = new Date().getTime();
   try {
-    const Name = `sac_${new Date().getTime()}.jpg`;
-    var base64Data = req.body.payload.replace(/^data:image\/png;base64,/, "");
-    base64Data = base64Data.replace(/(\r\n|\n|\r)/gm, "");
-    const sharp = require("sharp");
-    var toUint8Array = require("base64-to-uint8array");
-    var rekognition = new AWS.Rekognition({ region: "us-east-1" });
-    // var bufferValue = Buffer.from(base64Data, "base64");
-    var bufferValue = toUint8Array(base64Data);
-    bufferValue = await sharp(bufferValue)
-      .resize({ fit: "fill", width: 80, height: 80 })
+    const Name = `image_${new Date().getTime()}.jpg`;
+    var bufferValue = req.files.payload.data;
+    var rekognition = new AWS.Rekognition({ region: process.env.region });
+    bufferValue = await require("sharp")(bufferValue)
+      .resize({ fit: "fill", width: 100, height: 100 })
       .toBuffer();
     rekognition.detectCustomLabels(
       {
         Image: {
           Bytes: bufferValue,
         },
-        ProjectVersionArn:
-          "arn:aws:rekognition:us-east-1:655053471542:project/cac_loai_sac/version/cac_loai_sac.2020-08-21T17.14.49/1598004889458",
+        ProjectVersionArn: process.env.AWS,
         MinConfidence: 70,
       },
       (err, data) => {
         const timeLoad = new Date().getTime() - TimeStart;
         console.log(timeLoad);
-        console.log(data);
         res.json(data);
       }
     );
-    // require("fs").writeFile("images/" + Name, base64Data, "base64", function (
-    //   err
-    // ) {});
+    require("fs").writeFile(
+      "images/" + Name,
+      req.files.payload.data,
+      (err) => {}
+    );
   } catch (error) {
-    console.log(error);
-    res.send(error);
+    res.json({
+      msg: "" + error,
+    });
   }
 });
 app.listen(process.env.PORT || 5000);
